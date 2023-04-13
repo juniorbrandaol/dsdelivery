4b056f17ea0e89e3b892a4741c6eab02cb3c595e
junior brandao <edilson_brandaojunior@hotmail.com>
2023-04-13 19:10:32 -0300

    Project created

---
+++ backend/src/test/java/com/eblju/dsdelivery/DsdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eblju.dsdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
